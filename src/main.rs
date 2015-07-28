6de8b0f27a9a72b398e65506d91fb094cd12f627
ilkosta <costantino.giuliodori@gmail.com>
2015-07-28 11:38:09 +0200

    added logger and params for multi-part/formdata

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 
 extern crate crypto;
+extern crate iron;
+extern crate router;
+extern crate params;
+extern crate hyper;
+extern crate logger;
+
+
+use iron::prelude::*;
+use iron::{BeforeMiddleware, AfterMiddleware, typemap};
+use iron::middleware::Handler;
+use router::{Router};
+use iron::status;
+use params::Params;
+use logger::Logger;
+
+
 
 
 /// generate the md5sum of a given file
@@ -14,6 +30,28 @@ pub fn md5sum<T : std::io::Read> (f: &mut T) -> String  {
      digest.result_str()
 }
 
+
+
+fn submit_form_file(req: &mut Request) -> IronResult<Response> {
+    println!("cazzo");
+    println!("{:?}", req.get_ref::<Params>());
+    Ok(Response::with(status::Ok))
+}
+
+
+
+
+
+fn start_server() {
+   let (logger_before, logger_after) = Logger::new(None);
+   let mut router = Router::new();
+   let mut chain_form_file = Chain::new(submit_form_file);
+   chain_form_file.link_before(logger_before);
+   chain_form_file.link_after(logger_after);
+   router.post("/openact", chain_form_file);
+   println!("started server at http://localhost:3000/");
+   Iron::new(router).http("localhost:3000").unwrap();
+}
 
 
 #[cfg(test)]
@@ -34,10 +72,38 @@ mod tests {
         assert_eq!(md5sum(&mut odt), "a381cbe043c38a94af0983ffa934f338");
     }
 
+    #[test]
+    fn simple_get() {
+        use hyper::Client;
+        use hyper::header::Connection;
+        use std::io::Read;
+        use super::start_server;
+
+        // start_server();
+
+        // Create a client.
+        let client = Client::new();
+        //
+        // Creating an outgoing request.
+        let mut res = client.get("http://localhost:3000/openact")
+            // set a header
+            .header(Connection::close())
+            // let 'er go!
+            .send().unwrap();
+
+        // Read the Response.
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+
+        println!("Response: {}", body);
+        assert_eq!(body, "Hello World");
+    }
+
 }
 
 
 
 fn main() {
     println!("Hello, world!");
+    start_server();
 }
