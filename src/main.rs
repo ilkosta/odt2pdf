9e0a66058b88a0d7ebc0cbb8e81be5f8d8e49152
ilkosta <costantino.giuliodori@gmail.com>
2015-08-05 12:01:14 +0200

    handled the file as filename parameter of the request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+﻿
 extern crate crypto;
 extern crate iron;
 extern crate router;
@@ -14,27 +14,86 @@ use router::{Router};
 use iron::status;
 use params::Params;
 use logger::Logger;
+use std::error::Error;
 
 
 
-
-/// generate the md5sum of a given file
+/// generate the md5sum of a given file (io::Read)
 pub fn md5sum<T : std::io::Read> (f: &mut T) -> String  {
-    use crypto::digest::Digest;
+  use crypto::digest::Digest;
 
-     let mut digest = crypto::md5::Md5::new();
-     let mut data: Vec<u8> = Vec::new();
+  let mut digest = crypto::md5::Md5::new();
+  let mut data: Vec<u8> = Vec::new();
 
-     f.read_to_end(&mut data);
-     digest.input(&data);
-     digest.result_str()
+  f.read_to_end(&mut data);
+  digest.input(&data);
+  digest.result_str()
 }
 
+// fn getFile(params: &params::Map) -> Option<&std::io::Read> 
+// {
+//   use std::fs::File;
+//   use std::path::Path;
+//   
+// //   #[cfg(debug)]
+// //   {
+// //     for (key, value) in params.iter() {
+// //       println!("{}: {:?}", key, value);
+// //     }
+// //   }
+//   
+//   
+//   match params.find(&["filename"]) {
+//     Some(ref f) => println!("{:?}", f),
+//     
+//                       { // Open the path in read-only mode, returns `io::Result<File>`
+//       let path = Path::new(path);
+//       match File::open(path) {
+//           // The `description` method of `io::Error` returns a string that
+//           // describes the error
+//           Err(why) => panic!("couldn't open {}: {}", path.display(),
+//                                                     Error::description(&why)),
+//           Ok(file) => &file,
+//       }
+//     },
+//     Nome => //None
+//             println!("peccato"),
+//   }
+// }
 
-
+// curl -i "http://localhost:3000/openact"  -F "filename=@/home/costa/test.csv" -F "name=jason" -F
+// "age=2"
 fn submit_form_file(req: &mut Request) -> IronResult<Response> {
-    println!("cazzo");
-    println!("{:?}", req.get_ref::<Params>());
+  let param_res = req.get_ref::<Params>(); // Result<&params::Map, params::ParamsError>
+    println!("{:?}", &param_res);
+    
+    let md5sum_check = match param_res {
+        Ok(ref params) => match params.find(&["md5sum"]) {
+          Some(ref passed_md5sum) => {
+            match params.find(&["filename"]) {
+              None => {
+                println!("missing 'filename' field: {:?}", params);
+                false
+              },
+              Some(f) => { 
+                match *f {
+                  params::Value::File(ref file) => println!("the path is: {:?}", file.path() ),
+                  _ => println!("non si tratta di un file!!!!")
+                }
+                true
+              }
+//               match *f as params::Value {
+//                 params::Value::File => {
+//                   let file = f as &params::Value::File;
+//                   md5sum(file.open()) == passed_md5sum
+//                 }
+//               }
+            }
+          },
+            _ => false
+        },
+        Err(why) => {println!("{:?}", why); false},
+    };
     Ok(Response::with(status::Ok))
 }
 
@@ -60,7 +119,7 @@ mod tests {
     use std::fs::File;
     use super::md5sum;
 
-    #[test]
+#[test]
     fn md5sum_of_file() {
         static FP : &'static str = "/home/costa/pkg/lav/regione/attiweb/attività/20150107/definizione procedura di riavvio/Procedure_Riavvio_omnia.odt";
 
