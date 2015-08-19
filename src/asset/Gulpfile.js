6f7561f4265646c102ca1c3fe3ab450ce2beb2bc
ilkosta <costantino.giuliodori@gmail.com>
2015-08-19 11:35:27 +0200

    some js fix for the example form

--- src/asset/Gulpfile.js
+++ src/asset/Gulpfile.js
@@ -1,5 +1,6 @@
 ﻿var gulp = require('gulp');
 var livereload = require('gulp-livereload');
+var jshint = require('gulp-jshint');
 
 var EXPRESS_PORT = 4000;
 var EXPRESS_ROOT = __dirname;
@@ -25,13 +26,20 @@ function startExpress() {
 function notifyLivereload(event) {
 
   gulp.src(event.path, {read: false})
-  .pipe(livereload({ start: true }));
-  console.log('notified', event);
+      .pipe(livereload({ start: true }));
 }
+
+function lint(event) {
+  return gulp.src('./html/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+}
+
 
 gulp.task('default', function () {
 
   startExpress();
   livereload.listen();
   gulp.watch('html/*.html', notifyLivereload);
+  gulp.watch('html/js/*.js', lint);
 });
